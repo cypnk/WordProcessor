18777a6b1296d3ec79b044d1feb732cf8d3731dc
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2018-12-10 12:40:28 -0500

    Added cursor prep and key combo prototype

--- software/Huxley/src/headers/Editor.h
+++ software/Huxley/src/headers/Editor.h
@@ -1,6 +1,7 @@
 #ifndef EDITOR_H
 #define EDITOR_H
 
+#include <SDL2/SDL.h>
 
 /**
  *  Text input
@@ -8,6 +9,11 @@
 extern char* input;
 extern char* composition;
 
+extern Sint32 cursor;
+extern Sint32 selection;
+
+void
+sendCombo( int ctrl, int shift, const char* c );
 
 #endif
 
