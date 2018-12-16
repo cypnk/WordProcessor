6b9070363c70b432328b1d65f27560a5cf9a4a90
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2018-12-16 13:35:32 -0500

    Moved out command map, converted to class

--- software/Huxley/src/headers/Editor.h
+++ software/Huxley/src/headers/Editor.h
@@ -4,122 +4,16 @@
 #include <stdio.h>
 #include <vector>
 #include <SDL2/SDL.h>
+#include "HXTypes.h"
 
-/**
- *  Keyboard commands
- */
+// Idea borrowed from Salvatore Sanfilippo's ( antirez ) Kilo editor
+// https://github.com/antirez/kilo
+#define IS_BREAK( c ) \
+	( c == '\0' || isspace( c ) || strchr( END_MKR, c ) != NULL )
 
-/**
- *  Move commands
- */
-
-// Basic movement
-#define	M_UP		0x0000
-#define	M_DOWN		0x0001
-#define	M_LEFT		0x0002
-#define M_RIGHT		0x0003
-
-// Line movement
-#define M_LNSTART	0x0004
-#define M_LNEND		0x0005
-
-// Scrolling
-#define M_SCRLUP	0x0006
-#define M_SCRLDN	0x0007
-
-// Pagination
-#define M_PGUP		0x0008
-#define M_PGDN		0x0009
-
-// Document start/end
-#define M_DSTART	0x0010
-#define M_DEND		0x0011
-
-// Text selections
-#define S_LEFT		0x0012
-#define S_RIGHT		0x0013
-
-// Find text
-#define T_QUERY		0x0014
-
-/**
- *  Editing commands
- */
-
-// Delete left/right of cursor
-#define	E_DELL		0x0015
-#define	E_DELR		0x0016
-
-// Delete current word/line
-#define	E_DELWD		0x0017
-#define	E_DELLN		0x0018
-
-// Insert page break
-#define E_BREAK		0x0019
-
-// Copy to cliboard
-#define C_COPY		0x0020
-#define C_PASTE		0x0021
-
-// Insert memo
-#define P_MEMO		0x0022
-
-// Create subroutine
-#define P_PROG		0x0023
-
-// History undo/redo
-#define H_UNDO		0x0024
-#define H_REDO		0x0025
-
-// Document handling
-#define T_NEW		0x0026
-#define T_OPEN		0x0027
-
-// Indent
-#define T_INDENT	0x0028
-#define T_ODENT		0x0029
-
-// Superscript/subscript
-#define T_SUP		0x0030
-#define T_SUB		0x0031
-
-// Basic formatting
-#define T_BOLD		0x0032
-#define T_ITALIC	0x0033
-#define T_UNDER		0x0034
-
-// Return cursor
-#define M_CUR		0x0035
-#define M_LCUR		0x0036
-
-// Open clipboard
-#define C_CLIP		0x0037
-
-// Insert break
-#define T_BREAK		0x0038
-
-
-/**
- *  Formatting type
- */
-#define	F_NORMAL	0x0100;
-#define	F_BOLD		0x0101;
-#define	F_ITALIC	0x0102;
-#define	F_UNDER		0x0103;
-#define	F_SUP		0x0104;
-#define	F_SUB		0x0105;
-
-/**
- *  Helpers
- */
-#define ARRAY_SIZE( a ) \
-	( sizeof( a ) / sizeof( *( a ) ) )
-
-// Columns per screen
-#define COL_SIZE 120
-
-// Lines per page
-#define PG_SIZE	62
+// Word separator ( for languages that use spaces )
+#define IS_SPACE( c ) \
+	( strchr( " ", c ) != NULL )
 
 /**
  *  Text input
@@ -127,15 +21,32 @@
 extern char* input;
 extern char* composition;
 
-void
-sendCombo( int ctrl, int shift, SDL_Keycode &code );
-
-void
-applyCommand( unsigned char action );
-
-void
-cmdOpen( const char* fname );
-
+class Editor {
+	private:
+		
+		// Current working document
+		HX_FILE	working_doc;
+		
+		// Last cusror position
+		std::vector<HX_CURSOR> cur_history;
+		
+		// Keyboard input
+		std::vector<HX_HISTORY> content;
+		
+		//	Open a given filename as a Huxley document
+		void cmdOpen( const char* fname );
+		
+	public:
+		Editor( unsigned char key_map );
+		
+		void	capslock();
+		
+		// Receive keyboard key combination
+		void
+		sendCombo( int ctrl, int shift, SDL_Keycode &code );
+		
+		// Apply requested command
+		void	applyCommand( unsigned char action );
+};
 
 #endif
-
