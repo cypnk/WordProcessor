d0e37d7b3bdc30ced0f9fab6a7da4816bc46ee05
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2018-12-17 11:59:26 -0500

    Added file opening methods. Renamed from File.cpp to HXFile.cpp

---
+++ software/Huxley/src/HXFile.cpp
@@ -0,0 +1,95 @@
+#ifndef HXFILE_CPP
+#define HXFILE_CPP
+
+#include "headers/HXFile.h"
+
+//int ENDS_WITH( const char* str, const char* suffix );
+
+
+HXFile::HXFile() { }
+
+bool
+HXFile::openFile( std::ifstream &file ) {
+	if( file.is_open() && file.good() ) {
+		return true;
+	}
+	
+	return false;
+}
+
+void
+HXFile::openTextDoc( const char* fname, HX_FILE& dest ) {
+	// Setup working file
+	std::ifstream file( fname );
+	if ( !openFile( file ) ) {
+		return;
+	}
+	
+	std::string line;
+	// Append lines to document block
+	while ( std::getline( file, line ) ) {
+		appendDoc( line, dest );
+		line.clear();
+	}
+	
+	file.close();
+	if ( !file ) {
+		// Error
+		return;
+	}
+}
+
+void
+HXFile::openHuxleyDoc( const char* fname, HX_FILE& dest ) {
+	// Setup working file
+	std::ifstream file( fname );
+	if ( !openFile( file ) ) {
+		return;
+	}
+	
+	
+	std::string line;
+	// Append lines to document block
+	while ( std::getline( file, line ) ) {
+		appendDoc( line, dest );
+		line.clear();
+	}
+	
+	file.close();
+	if ( !file ) {
+		// Error
+		return;
+	}
+}
+
+void
+HXFile::openDoc( const char* fname, HX_FILE& dest ) {
+	// Detect file type and load Huxley documents differently
+	if ( ENDS_WITH( fname, ".txt" ) ) {
+		openTextDoc( fname, dest );
+	} else if ( ENDS_WITH( fname, ".hx" ) ) {
+		openHuxleyDoc( fname, dest );
+	} else {
+		openTextDoc( fname, dest );
+	}
+}
+
+void
+HXFile::appendDoc( std::string& line, HX_FILE& dest ) {
+	// TODO: Make this read formatting from the line
+	std::vector<HX_FORMAT> fmt;
+	
+	// Add line formatting
+	fmt.push_back( HX_FORMAT{ 0, 0, 0x0000 } );
+	
+	// Add line with any formatting and its checksum
+	dest.data.push_back( 
+		HX_LINE {
+			std::hash<std::string>{}( line ),
+			line,
+			fmt
+		}
+	);	
+}
+
+#endif
