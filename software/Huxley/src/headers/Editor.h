14fb63b0d471b29aa2f042816d0817dad4b9dfc7
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2018-12-17 11:50:36 -0500

    Removed macros, added key map

--- software/Huxley/src/headers/Editor.h
+++ software/Huxley/src/headers/Editor.h
@@ -3,17 +3,9 @@
 
 #include <stdio.h>
 #include <vector>
+#include <algorithm>
 #include <SDL2/SDL.h>
 #include "HXTypes.h"
-
-// Idea borrowed from Salvatore Sanfilippo's ( antirez ) Kilo editor
-// https://github.com/antirez/kilo
-#define IS_BREAK( c ) \
-	( c == '\0' || isspace( c ) || strchr( END_MKR, c ) != NULL )
-
-// Word separator ( for languages that use spaces )
-#define IS_SPACE( c ) \
-	( strchr( " ", c ) != NULL )
 
 /**
  *  Text input
@@ -23,6 +15,9 @@ extern char* composition;
 
 class Editor {
 	private:
+	
+		// Current keyboard map
+		command *KEY_MAP;
 		
 		// Current working document
 		HX_FILE	working_doc;
