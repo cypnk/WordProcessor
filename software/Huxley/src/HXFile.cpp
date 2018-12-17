40c632b296c866cebec5bf46451f2221f09f806b
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2018-12-17 14:14:56 -0500

    Consolidated opening, added checksum verification

--- software/Huxley/src/HXFile.cpp
+++ software/Huxley/src/HXFile.cpp
@@ -15,78 +15,120 @@ HXFile::openFile( std::ifstream &file ) {
 }
 
 void
-HXFile::openTextDoc( const char* fname, HX_FILE& dest ) {
-	// Setup working file
-	std::ifstream file( fname );
-	if ( !openFile( file ) ) {
-		return;
-	}
+HXFile::extractLine(
+	std::size_t&	chk,
+	std::string&	line,
+	std::string&	extracted
+) {
+	std::istringstream str( line );
+	std::string block;
+	int i = 0;
 	
-	std::string line;
-	// Append lines to document block
-	while ( std::getline( file, line ) ) {
-		appendDoc( line, dest );
-		line.clear();
-	}
-	
-	file.close();
-	if ( !file ) {
-		// Error
-		return;
-	}
-}
-
-void
-HXFile::openHuxleyDoc( const char* fname, HX_FILE& dest ) {
-	// Setup working file
-	std::ifstream file( fname );
-	if ( !openFile( file ) ) {
-		return;
-	}
-	
-	// TODO: This needs to be handled very differently
-	std::string line;
-	// Append lines to document block
-	while ( std::getline( file, line ) ) {
-		appendDoc( line, dest );
-		line.clear();
-	}
-	
-	file.close();
-	if ( !file ) {
-		// Error
-		return;
-	}
-}
-
-void
-HXFile::openDoc( const char* fname, HX_FILE& dest ) {
-	// Detect file type and load Huxley documents differently
-	if ( ENDS_WITH( fname, ".txt" ) ) {
-		openTextDoc( fname, dest );
-	} else if ( ENDS_WITH( fname, ".hx" ) ) {
-		openHuxleyDoc( fname, dest );
-	} else {
-		openTextDoc( fname, dest );
-	}
-}
-
-void
-HXFile::appendDoc( std::string& line, HX_FILE& dest ) {
-	// TODO: Make this read formatting from the line
-	std::vector<HX_FORMAT> fmt;
-	
-	// Add line formatting
-	fmt.push_back( HX_FORMAT{ 0, 0, 0x0000 } );
-	
-	// Add line with any formatting and its checksum
-	dest.data.push_back( 
-		HX_LINE {
-			std::hash<std::string>{}( line ),
-			line,
-			fmt
+	while( std::getline( str, block, ' ' ) ) {
+		// Checksum
+		if ( i == 0 ) {
+			// Copy checksum
+			std::sscanf( block.c_str(), "%zx", &chk );
+		
+		// TODO: Extract formatting
+		//} else if ( i == 1 ) {
+			
+		} else {
+			extracted.append( block + ' ' );
 		}
-	);	
+		i++;
+	}
+	// Remove lastly appened space
+	if ( extracted.length() > 1 ) {
+		extracted.erase( extracted.length() - 1 );
+	}
+}
+
+void
+HXFile::openDoc( std::string& fname, HX_FILE& dest ) {
+	const char* fcmp	= fname.c_str();
+	
+	// Setup working file
+	std::ifstream file( fcmp );
+	if ( !openFile( file ) ) {
+		return;
+	}
+	
+	std::string line;
+	// Detect file type and load Huxley documents differently
+	if ( ENDS_WITH( fcmp, ".txt" ) ) {
+		// Append lines to document block
+		while ( std::getline( file, line ) ) {
+			appendDoc( line, dest, FILE_TEXT );
+			line.clear();
+		}
+		
+	} else if ( ENDS_WITH( fcmp, ".hx" ) ) {
+		// Append Huxley line to document block
+		while ( std::getline( file, line ) ) {
+			appendDoc( line, dest, FILE_HUXLEY );
+		}
+	} else {
+		// Shouldn't handle yet
+		while ( std::getline( file, line ) ) {
+			appendDoc( line, dest, FILE_UNKOWN );
+			line.clear();
+		}
+		
+	}
+	
+	file.close();
+	if ( !file ) {
+		// Error
+		return;
+	}
+}
+
+void
+HXFile::appendDoc(
+	std::string&	line, 
+	HX_FILE& 	dest,
+	unsigned char	ftype
+) {
+	// TODO: Make this read formatting from the line
+	std::vector<HX_FORMAT>	fmt;
+	std::size_t		chk;
+	
+	switch( ftype ) {
+		case FILE_HUXLEY: {
+			std::string extracted	= "";
+			
+			// Extract line and checksum
+			extractLine( chk, line, extracted );
+			
+			// TODO: Extract line formatting
+			fmt.push_back( HX_FORMAT{ 0, 0, 0x0000 } );
+			
+			// Add line with any formatting
+			// including given and calculated checksum
+			dest.data.push_back( HX_LINE {
+				chk,
+				std::hash<std::string>{}( extracted ),
+				extracted, 
+				fmt
+			} );
+			break;
+		} 
+		
+		default: {
+			// Fresh checksum
+			chk	= std::hash<std::string>{}( line );
+			
+			// Empty formatting
+			fmt.push_back( HX_FORMAT{ 0, 0, 0x0000 } );
+			
+			// Add line
+			dest.data.push_back( HX_LINE { 
+					chk, chk, line, fmt 
+			} );
+			break;
+		}
+	}
 }
 
 #endif
