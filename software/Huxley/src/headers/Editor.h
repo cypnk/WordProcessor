bd444f9091fca1b24725c5bdb9adec9f4818cd81
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2018-12-18 21:59:12 -0500

    Added direct input handling, working string

--- software/Huxley/src/headers/Editor.h
+++ software/Huxley/src/headers/Editor.h
@@ -22,29 +22,39 @@ class Editor {
 		// Current working document
 		HX_FILE	working_doc;
 		
+		// Current working input
+		std::string working_str;
+		
 		// Last cusror position
 		std::vector<HX_CURSOR> cur_history;
 		
 		// Keyboard input
 		std::vector<HX_HISTORY> content;
 		
+		// Add last input phrase to history
+		void	syncInput();
+		
 		// Open a given filename as a Huxley document
-		void cmdOpen( std::string& fname );
+		void	cmdOpen( std::string& fname );
 		
 		// Save working doument to filename
-		void cmdSave( std::string& fname );
+		void	cmdSave( std::string& fname );
+		
+		// Apply requested command
+		void	applyCommand( unsigned char action );
 		
 	public:
 		Editor( unsigned char key_map );
 		
 		void	capslock();
 		
+		// Receive keyboard input
+		void
+		sendInput( char* edit, Sint32 cursor, Sint32 len );
+		
 		// Receive keyboard key combination
 		void
 		sendCombo( int ctrl, int shift, SDL_Keycode &code );
-		
-		// Apply requested command
-		void	applyCommand( unsigned char action );
 };
 
 #endif
