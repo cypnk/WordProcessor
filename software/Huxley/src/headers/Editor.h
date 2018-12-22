18cc5747c0eacea4e3e6688242b8655e114b7611
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2018-12-22 04:57:21 -0500

    Moved HXFile stuff inward

--- software/Huxley/src/headers/Editor.h
+++ software/Huxley/src/headers/Editor.h
@@ -1,15 +1,29 @@
 #ifndef EDITOR_H
 #define EDITOR_H
 
+#include <cstring>
+#include <string>
+#include <iterator>
 #include <stdio.h>
 #include <vector>
+#include <fstream>
+#include <sstream>
 #include <algorithm>
 #include <SDL2/SDL.h>
 #include "HXTypes.h"
 
+// Checksum primes
+#define	CHK_A		81667
+#define	CHK_B		83311
+#define	CHK_C		90173
+
+
 class Editor {
 	private:
-	
+		
+		// Collection for corrupted lines
+		std::vector<std::size_t> bad_lines;
+		
 		// Current keyboard map
 		HX_COMMAND *KEY_MAP;
 		
@@ -44,6 +58,50 @@ class Editor {
 		void	workingLimit( std::size_t& index );
 		
 		/**
+		 *  Client input handling
+		 */
+				// Generate checksum from string
+		static std::size_t
+		toCHK( std::string& line );
+		
+		static int 
+		endsWith(
+			const char*	str,
+			const char*	suffix
+		);
+		
+		// Copy from string to checksum size_t
+		static void
+		fromCHK(
+			std::string&		block, 
+			std::size_t&		chk
+		);
+		
+		// Copy checksum to char array
+		static void 
+		copyCHK(
+			char*			check,
+			std::size_t&		chk
+		);
+		
+		/**
+		 *  Separatator detection
+		 */
+		static bool	isBreak( int c );
+		static bool	isSpace( const char* c );
+		
+		/**
+		 *  Content chunking
+		 */
+		static void
+		breakSegments( 
+			std::string&			working, 
+			std::vector<std::string>&	segments,
+			bool				append	= false
+		);
+		
+		
+		/**
 		 *  Flags
 		 */
 		// Unicode text input active
@@ -52,10 +110,73 @@ class Editor {
 		// Apply requested command
 		void	applyCommand( unsigned char action );
 		
+		// Open file
+		void
+		openDoc( std::string& fname, HX_FILE& dest );
+		
+		// Save file
+		void
+		saveDoc( std::string& fname, HX_FILE& source );
+		
+		/**
+		 *  Data handling
+		 */
+		
+		// Convert from raw string to format stack
+		void
+		extractFormat(
+			std::string&	block,
+			std::vector<HX_FORMAT>& fmt 
+		);
+		
+		// Convert from raw string to document line
+		void
+		extractLine(
+			std::size_t&	chk,
+			std::string&	line,
+			std::string&	extracted,
+			std::vector<HX_FORMAT>& fmt
+		);
+		
+		// Process line segments
+		void
+		segmentsToLine( 
+			std::vector<std::string>&	segments, 
+			std::size_t&			index,
+			std::size_t&			chk,
+			std::vector<HX_FORMAT>&		fmt,
+			HX_FILE& 			dest
+		);
+		void
+		segmentsToFile( 
+			std::vector<std::string>&	segments, 
+			std::size_t&			index,
+			std::size_t&			chk,
+			std::vector<HX_FORMAT>&		fmt,
+			HX_FILE& 			dest
+		); 
+		
+		// Convert from format stack to string
+		void
+		saveFormat(
+			std::string&	block,
+			std::vector<HX_FORMAT>& fmt 
+		);
+		
+		void
+		appendDoc(
+			std::string&	line, 
+			HX_FILE& 	dest,
+			std::size_t	index,
+			unsigned char	ftype
+		);
 	public:
 		Editor( unsigned char key_map );
 		
 		void	capslock();
+		
+		// Checksum passed
+		bool			good	= true;
 		
 		// Open a given filename as a Huxley document
 		void	cmdOpen( std::string& fname );
