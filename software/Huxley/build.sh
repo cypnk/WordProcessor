db2724b0fe829c797e56d9002e79bc29297dcbf2
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-02-08 04:24:21 -0500

    Update build.sh

--- software/Huxley/build.sh
+++ software/Huxley/build.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# Binary destination and code source
+BD=bin
+SC=src
+
+# Timestamp
+DATE=`date +%Y-%m-%d-%H-%M-%S`
+
 # File presets
-IF=bin/huxley
-OF=bin/huxley.sha1
+IF=$BD/huxley
+OF=$BD/huxley.sha1
+
+# Document storage
+DB=$BD/data.db
+SQ=$SC/sql/data.sql
+
+# Backup of document storage
+BK=data-backup-$DATE.sql
+
+# Make bin direcory
+mkdir -p $BD
 
 # Make and clean
 make && make clean
@@ -13,6 +30,14 @@ sha1sum $IF > $OF
 # Display
 echo ''
 echo 'Build complete'
+echo ''
+if [ -f $DB ]; then
+	sqlite3 $DB .dump > $BK
+	echo 'Database backed up'
+else
+	sqlite3 $DB < $SQ
+	echo 'Database created'
+fi
 echo ''
 cat $OF
 
